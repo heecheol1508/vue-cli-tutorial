f9e96d142fd3edd079edaf97d4100a8269474880
TIL <52685275+heecheol1508@users.noreply.github.com>
2020-12-06 20:37:24 +0900

    [vue.js] Control Data

---
+++ test/src/views/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <h1>Today is {{ title1 }}</h1>
+    <input type="text" v-model="input1" />
+    <button @click="getData">Get</button>
+    <button @click="setData">Set</button>
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option :key="i" :value="d.v" v-for="(d, i) in options">{{ d.t }}</option>
+    </select>
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="i" v-for="(d, i) in options">
+        <td>{{ d.v }}</td>
+        <td>{{ d.t }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      title1: "기술면접",
+      title2: "임원면접",
+      input1: "message",
+      options: [
+        { v: "S", t: "Seoul" },
+        { v: "J", t: "Jeju" },
+        { v: "B", t: "Busan" },
+      ],
+      region: "J",
+      tableShow: false,
+    };
+  },
+  watch: {
+    input1() {
+      console.log(this.input1);
+    },
+  },
+  methods: {
+    getData() {
+      alert(this.input1);
+    },
+    setData() {
+      this.input1 = "12345";
+    },
+    changeRegion() {
+      alert(this.region);
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    // console.log("beforeUpdate");
+  },
+  updated() {
+    // console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
+  },
+};
+</script>
