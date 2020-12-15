66a24f5ad39799680dbbeeb000715a202c4ceea8
heecheol <heecheol1508@gmail.com>
2020-12-15 14:35:17 +0900

    Study Props

---
+++ 01_props/props_and_emit/src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="blue lighten-3 pa-3">
+    <h1>User 컴포넌트</h1>
+    <p>이름: {{ name }}</p>
+    <hr />
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <UserDetail
+          :name="name"
+          :address="address"
+          :phone="phone"
+          :hasDog="hasDog"
+        ></UserDetail>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <UserEdit
+          :name="name"
+          :address="address"
+          :phone="phone"
+          :hasDog="hasDog"
+          @child="parents"
+        ></UserEdit>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  components: {
+    UserDetail,
+    UserEdit,
+  },
+  data() {
+    return {
+      name: "Hoza",
+      address: "Seoul",
+      phone: "1234-5678",
+      hasDog: true,
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "아비투스";
+    },
+    parents(user) {
+      this.name = user.name;
+      this.address = user.address;
+      this.phone = user.phone;
+      this.hasDog = user.hasDog;
+    },
+  },
+};
+</script>
